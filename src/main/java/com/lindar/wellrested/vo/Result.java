2aaafd99d7508112dceb756202ad2081cc64bebd
Iulian Dafinoiu <cicorongho@yahoo.com>
2017-03-17 11:16:21 +0000

    improve working with Result objects by adding filter and flatMap methods

--- src/main/java/com/lindar/wellrested/vo/Result.java
+++ src/main/java/com/lindar/wellrested/vo/Result.java
@@ -1,12 +1,14 @@
 package com.lindar.wellrested.vo;
 
 import java.io.Serializable;
-import java.util.List;
 import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import lombok.Value;
 import org.apache.commons.lang3.StringUtils;
 
+@Value
 public class Result<T> implements Serializable {
 
     private static final long serialVersionUID = 1987956456765489787L;
@@ -36,13 +38,18 @@ public class Result<T> implements Serializable {
     public boolean isSuccess() {
         return success;
     }
-
-    public T getData() {
-        return data;
+    
+    public boolean isSuccessAndNotNull() {
+        return success && data != null;
     }
 
-    public String getMsg() {
-        return msg;
+    public Result<T> filter(Predicate<? super T> predicate) {
+        Objects.requireNonNull(predicate);
+        if (!isSuccess()) {
+            return this;
+        } else {
+            return predicate.test(data) ? this : ResultFactory.getFailResult(msg);
+        }
     }
 
     public <U> Result<U> map(Function<? super T, ? extends U> mapper) {
@@ -54,6 +61,15 @@ public class Result<T> implements Serializable {
         }
     }
 
+    public <U> Result<U> flatMap(Function<? super T, Result<U>> mapper) {
+        Objects.requireNonNull(mapper);
+        if (!isSuccess()) {
+            return ResultFactory.getFailResult(msg);
+        } else {
+            return Objects.requireNonNull(mapper.apply(data));
+        }
+    }
+
     public void ifSuccess(Consumer<? super T> consumer) {
         if (data != null) {
             consumer.accept(data);
@@ -62,64 +78,5 @@ public class Result<T> implements Serializable {
 
     public T orElse(T other) {
         return data != null ? data : other;
-    }
-
-    @Override
-    public String toString() {
-
-        StringBuilder sb = new StringBuilder("\"Result{\"");
-        sb.append("success=").append(success);
-        sb.append(", msg=").append(msg);
-
-        sb.append(", data=");
-
-        if (data == null) {
-
-            sb.append("null");
-
-        } else if (data instanceof List) {
-
-            List castList = (List) data;
-            if (castList.isEmpty()) {
-
-                sb.append("empty list");
-
-            } else {
-                Object firstItem = castList.get(0);
-
-                sb.append("List of ").append(firstItem.getClass());
-            }
-
-        } else {
-            sb.append(data.toString());
-        }
-
-        sb.append("}");
-
-        return sb.toString();
-
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 89 * hash + (this.success ? 1 : 0);
-        hash = 89 * hash + Objects.hashCode(this.data);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Result<?> other = (Result<?>) obj;
-        if (this.success != other.success) {
-            return false;
-        }
-        return Objects.deepEquals(this.data, other.data);
     }
 }
